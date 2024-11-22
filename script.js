b8b3d2db312686ac6225777d574975ddb7bb9919
Gustavo Barboza <160795606+gustavonbarboza@users.noreply.github.com>
2024-11-22 16:23:56 -0300

    cleanup comments script.js

--- script.js
+++ script.js
@@ -1,46 +1,38 @@
-// Seleciona todos os links do menu com href começando com #
 const menuLinks = document.querySelectorAll('a[href^="#"]');
 
-// Adiciona o evento de clique a cada link
 menuLinks.forEach(link => {
   link.addEventListener('click', function (e) {
-    e.preventDefault(); // Previne o comportamento padrão
+    e.preventDefault();
 
-    // Seleciona o elemento correspondente ao ID no href
-    const targetId = this.getAttribute('href'); // Exemplo: #menu ou #projetos
+    const targetId = this.getAttribute('href');
     const targetElement = document.querySelector(targetId);
 
-    // Faz o scroll suave para o elemento
     window.scrollTo({
-      top: targetElement.offsetTop, // Calcula a posição vertical
-      behavior: 'smooth', // Scroll suave
+      top: targetElement.offsetTop,
+      behavior: 'smooth',
     });
   });
 });
 
-// Seleciona os elementos
-const images = document.querySelectorAll('.projeto-img'); // Todas as imagens dos cards
+const images = document.querySelectorAll('.projeto-img'); 
 const modal = document.getElementById('image-modal');
 const modalImage = document.getElementById('modal-image');
 const closeBtn = document.querySelector('.close-btn');
 
-// Adiciona evento de clique em cada imagem
 images.forEach(image => {
   image.addEventListener('click', () => {
-    modal.style.display = 'flex'; // Exibe o modal
-    modalImage.src = image.src;  // Define o src da imagem clicada
+    modal.style.display = 'flex';
+    modalImage.src = image.src;
   });
 });
 
-// Fecha o modal ao clicar no botão de fechar
 closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none'; // Esconde o modal
+  modal.style.display = 'none'; 
 });
 
-// Fecha o modal ao clicar fora da imagem
 modal.addEventListener('click', (e) => {
   if (e.target === modal) {
-    modal.style.display = 'none'; // Esconde o modal
+    modal.style.display = 'none';
   }
 });
 
